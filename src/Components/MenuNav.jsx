f7ac58e5484cd7fbf017e55c5c0cbe37e795e13e
edgarbp95 <edgarbp95@hotmail.com>
2023-04-19 10:25:07 -0300

    ajuste de navbar

--- src/Components/MenuNav.jsx
+++ src/Components/MenuNav.jsx
@@ -6,6 +6,7 @@ import { HiHome } from 'react-icons/hi';
 import { MdHowToVote, MdCollections } from 'react-icons/md';
 import {AiOutlineArrowDown} from "react-icons/ai";
 import {IoIosArrowDown} from "react-icons/io";
+import {BsArrowRightShort} from "react-icons/bs"
 
 const MenuNav = ({removeAct, addAct, refActive}) => {
 
@@ -14,7 +15,8 @@ const MenuNav = ({removeAct, addAct, refActive}) => {
         weekly: false,
         montly: false,
         private: false,
-        vispx: false
+        vispx: false,
+        claim: false
     })
 
     const toggleEarn = (id)=>{
@@ -35,6 +37,8 @@ const MenuNav = ({removeAct, addAct, refActive}) => {
         }else if(id=="vispx"){
             nEarn.vispx = !nEarn.vispx
             setIsOpen(nEarn)
+        }else if(id=="claim"){
+            nEarn.claim = !nEarn.claim
         }
     }
 
@@ -50,7 +54,7 @@ const MenuNav = ({removeAct, addAct, refActive}) => {
                             </span></a>
                         <ul className="collapse nav flex-column ms-1 menu-list w-100 pruebita" id="submenu2" data-bs-parent="#menu">
                             <li className="w-100 menu-list-group">
-                                <a onClick={()=>toggleEarn("weekly")} href="#submenu3" data-bs-toggle="collapse" className="px-0"> Weekly <IoIosArrowDown className={`${!isOpen.weekly ? "arrow-close" : "arrow-open"}`}/></a>
+                                <a onClick={()=>toggleEarn("claim")} href="#submenu3" data-bs-toggle="collapse" className="px-0"> Claim Only <IoIosArrowDown className={`${!isOpen.claim ? "arrow-close" : "arrow-open"}`}/></a>
                                 <ul className="collapse nav flex-column ms-3 pb-2" id="submenu3" data-bs-parent="#menu">
                                     <li className='w-100'>
                                         <NavLink onClick={addAct} to="/earn-strategies/weekly4"> Weekly 4</NavLink>
@@ -58,37 +62,103 @@ const MenuNav = ({removeAct, addAct, refActive}) => {
                                     <li className='w-100'>
                                         <NavLink onClick={addAct} to="/earn-strategies/weekly5"> Weekly 5</NavLink>
                                     </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/xborg2"> XBorg Holders 2</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/xborgcommunity"> XBorg Community claim</NavLink>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li className='w-100 menu-list-group'>
+                                 <NavLink onClick={addAct} to="/earn-strategies/earn-benftpass"> Earn BeNFT Pass</NavLink>
+                            </li>
+                            <li className="w-100 menu-list-group">
+                                <a onClick={()=>toggleEarn("weekly")} href="#submenu4" data-bs-toggle="collapse" className="px-0"> Weekly <IoIosArrowDown className={`${!isOpen.weekly ? "arrow-close" : "arrow-open"}`}/></a>
+                                <ul className="collapse nav flex-column ms-3 " id="submenu4" data-bs-parent="#menu">
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/weekly4"> Public Pool</NavLink>
+                                    </li>
                                 </ul>
                             </li>
                             <li className="w-100 menu-list-group">
-                                <a onClick={()=>toggleEarn("montly")} href="#submenu4" data-bs-toggle="collapse" className="px-0"> Montly <IoIosArrowDown className={`${!isOpen.montly ? "arrow-close" : "arrow-open"}`}/></a>
-                                <ul className="collapse nav flex-column ms-3" id="submenu4" data-bs-parent="#menu">
-                                    <li className='w-100'>Montly 1</li>
-                                </ul>
-                            </li>
-                            <li className="w-100 menu-list-group">
-                                <a onClick={()=>toggleEarn("private")} href="#submenu5" data-bs-toggle="collapse" className="px-0"> Private <IoIosArrowDown className={`${!isOpen.private ? "arrow-close" : "arrow-open"}`}/></a>
+                                <a onClick={()=>toggleEarn("montly")} href="#submenu5" data-bs-toggle="collapse" className="px-0"> Montly <IoIosArrowDown className={`${!isOpen.montly ? "arrow-close" : "arrow-open"}`}/></a>
                                 <ul className="collapse nav flex-column ms-3" id="submenu5" data-bs-parent="#menu">
-                                    <li className='w-100'>GH Private</li>
-                                    <li className='w-100'>LV Private</li>
-                                    <li className='w-100'>Lbank Private</li>
-                                    <li className='w-100'>Chip Private</li>
-                                    <li className='w-100'>Dale Private</li>
-                                    <li className='w-100'>SIV Private</li>
-                                    <li className='w-100'>Team Alex</li>
+                                    <NavLink onClick={addAct} to="/earn-strategies/monthly1">Monthly 1</NavLink>
+                                </ul>
+                            </li>
+                            <li className="w-100 menu-list-group">
+                                <a onClick={()=>toggleEarn("private")} href="#submenu6" data-bs-toggle="collapse" className="px-0"> Private <IoIosArrowDown className={`${!isOpen.private ? "arrow-close" : "arrow-open"}`}/></a>
+                                <ul className="collapse nav flex-column ms-3" id="submenu6" data-bs-parent="#menu">
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private1">GH Private</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private2">LV Private</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private4">Lbank Private</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private5">Chip Private</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private7">Dale Private</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private12">SIV Private</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private15">Team Alex</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private17">Piper M350x</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private22">Astrid</NavLink>
+                                    </li>
                                 </ul>
                             </li>
                             <li className="w-100 menu-list-group" >
-                                <a onClick={()=>toggleEarn("vispx")} href="#submenu6" data-bs-toggle="collapse" className="px-0"> Vispx <IoIosArrowDown className={`${!isOpen.vispx ? "arrow-close" : "arrow-open"}`}/></a>
-                                <ul className="collapse nav flex-column ms-3" id="submenu6" data-bs-parent="#menu">
-                                    <li className='w-100'>Vispx Core</li>
-                                    <li className='w-100'>Kickback Pool</li>
-                                    <li className='w-100'>BDE ventures</li>
-                                    <li className='w-100'>Crystal Tower</li>
-                                    <li className='w-100'>XBorg Holders</li>
-                                    <li className='w-100'>Xborg Holders 2</li>
-                                    <li className='w-100'>SCM</li>
-                                    <li className='w-100'>Friends with BeNFT</li>
+                                <a onClick={()=>toggleEarn("vispx")} href="#submenu7" data-bs-toggle="collapse" className="px-0"> Vispx <IoIosArrowDown className={`${!isOpen.vispx ? "arrow-close" : "arrow-open"}`}/></a>
+                                <ul className="collapse nav flex-column ms-3" id="submenu7" data-bs-parent="#menu">
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private6">Vispx Core</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private8">BDE ventures</NavLink>
+                                    </li>
+                                    
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/crystal-tower">Crystal Tower</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private10">XBorg Holders</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private11">SCM</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private14">Friends with BeNFT</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private16">VCX partner</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private23">XBorg Community</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private19">Neth</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private20">Degen Trading</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private21">Apes Together Strong</NavLink>
+                                    </li>
+                                    <li className='w-100'>
+                                        <NavLink onClick={addAct} to="/earn-strategies/private24">1% pool partner</NavLink>
+                                    </li>
                                 </ul>
                             </li>
                         </ul>
